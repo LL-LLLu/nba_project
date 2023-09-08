2dbb691af773446b587ca418a4d0e5d8b14ad7d6
LL-LLLu <qilutx@gmail.com>
2023-09-08 14:32:56 -0500

    banner text position

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -2,20 +2,24 @@ import React from 'react'
 import nba_banner from '../../public/video/nba_banner.mp4'
 
 const Banner = () => {
+
+
+
+
   return (
     <div className='mt-10 h-[1000px]'>
         <div>
-            <div className='relative'>
+            <div className='relative h-[400px] md:h-[700px]'>
                 <video
                     src={nba_banner}
                     autoPlay
                     loop
                     muted
-                    className='object-cover w-full h-[700px]'
+                    className='object-cover w-full h-full'
                 >
                 </video>
-                <div className='absolute left-[750px] top-[200px] text-center'>
-                    <div className='w-[600px] h-[300px] bg-black bg-opacity-30 backdrop-blur-lg text-center pt-[60px]'>
+                <div className='invisible md:visible absolute top-0 left-0 w-full h-full flex items-center justify-center'>
+                    <div className=' text-center w-[80%] md:w-[40%] bg-black bg-opacity-30 backdrop-blur-lg p-7'>
                         <h1 className='font-bold uppercase'>
                             Welcome to NBA Stats
                         </h1>
